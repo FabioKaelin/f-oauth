10458467726f95b3ba85dea55212358467a907b1
FabioKaelin <supermae206@gmail.com>
2023-06-29 22:18:44 +0200

    logout added

--- frontend-vue/src/components/layout.vue
+++ frontend-vue/src/components/layout.vue
@@ -7,15 +7,26 @@
         <!-- router link to /login -->
         <router-link to="/login">Login</router-link> |
         <!-- router link to /register -->
-        <router-link to="/register">Register</router-link>
+        <router-link to="/register">Register</router-link> |
+        <button @click="logout">Logout</button>
     </div>
 </template>
 
 <script lang="ts">
 
+import axios from 'axios';
 import { defineComponent } from 'vue'
+import { getAxiosConfig } from '../helper/request';
 
 export default defineComponent({
-    name: 'Layout'
+    name: 'Layout',
+    methods: {
+        logout() {
+            axios.request(getAxiosConfig('/auth/logout')).then((response) => {
+                console.log(response);
+                this.$router.push('/')
+            })
+        }
+    }
 })
 </script>
